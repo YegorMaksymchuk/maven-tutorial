344f29b74bb6647c768d7c66d6f795403be84350
Yegor Maksymchuk <ymaksymc@redhat.com>
2018-04-16 18:22:32 +0300

    refactoring

--- 002-jar-with-dependencies/src/main/java/com/company/main/Main.java
+++ 002-jar-with-dependencies/src/main/java/com/company/main/Main.java
@@ -2,6 +2,7 @@ package com.company.main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Make JAR, not WAR !!");
+		ProcessBuilder pb = new ProcessBuilder();
+		pb.command("java -version");
 	}
 }
