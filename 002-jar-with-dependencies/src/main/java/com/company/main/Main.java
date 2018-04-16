52c8f3c70b0fb0f28da61d290f57cd2a4ffedd8d
yemax <egormaksimchuk@gmail.com>
2018-04-16 19:17:35 +0300

    refactoring

--- 002-jar-with-dependencies/src/main/java/com/company/main/Main.java
+++ 002-jar-with-dependencies/src/main/java/com/company/main/Main.java
@@ -1,8 +1,16 @@
 package com.company.main;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
-		ProcessBuilder pb = new ProcessBuilder();
-		pb.command("java -version");
-	}
+    public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder("java", "-version");
+        pb.inheritIO();
+        try {
+            Process process = pb.start();
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
