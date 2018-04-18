cf991b9bdd59511938e62417981bf7777ca47d6b
Yegor Maksymchuk <ymaksymc@redhat.com>
2018-04-18 19:15:56 +0300

    refactoring

--- 002-jar-with-dependencies/src/main/java/com/company/main/Main.java
+++ 002-jar-with-dependencies/src/main/java/com/company/main/Main.java
@@ -1,5 +1,7 @@
 package com.company.main;
 
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
 		ProcessBuilder pb = new ProcessBuilder("java", "-version");
